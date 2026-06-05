568da6b48fa9c6c3d89e9bc103c7513152a4863e
Abhishek Kushwaha <ikushabhishek@gmail.com>
2026-06-05 17:11:38 +0530

    Initial commit - FinWise production stable split files

---
+++ sw.js
@@ -0,0 +1,45 @@
+﻿const CACHE_NAME = 'finwise-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Force immediate installation of the new service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Clean up old caches during activation phase
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Listen for the 'skipWaiting' message sent by the UI update pop-up banner
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
